eb7eee7b65a6ccffc08a5cf9a89ef9dfca5b0063
Deev Maksim <deev.maks2012@ya.ru>
2019-11-26 12:50:59 +0500

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,148 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <yandex-map
+      :settings="yamapSettings"
+      :coords="mapCenter"
+      ymap-class="map"
+      :controls="[]"
+      :placemarks="placemarks"
+      @click="onMapClick"
+    >
+
+      <ymap-marker
+        :key="index"
+        v-for="(item, index) in placemarks"
+        :coords="item.coordinates"
+        :marker-id="index"
+        :callbacks="{
+          click: onMarkerClick
+        }"
+      />
+
+    </yandex-map>
+
+    <div class="placemarks">
+      <div class="placemarks__title">
+        <h2>{{localization.list_title}}</h2>
+      </div>
+      <ul class="placemarks__list">
+        <li
+          v-for="(item, index) in placemarks"
+          :key="index"
+          :class="{active: isActivePlacemark(item)}"
+          @click="selectPlacemark(item.coordinates)"
+          class="placemarks__item"
+        >
+          <h6 class="item__title">{{localization.marker}} {{index}}</h6>
+          <span class="item__subtitle">[{{item.coordinates[0]}}, {{item.coordinates[1]}}]</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { yandexMap, ymapMarker } from 'vue-yandex-maps'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    yandexMap,
+    ymapMarker
+  },
+  data() {
+    return {
+      yamapSettings: this.$store.getters.YAMAP_SETTINGS,
+      mapCenter: this.$store.getters.MAP_CENTER,
+      localization: this.$store.getters.LOCALIZATION
+    }
+  },
+  computed: {
+    placemarks() {
+      return this.$store.getters.PLACEMARKS
+    },
+    selectedPlacemark() {
+      return this.$store.getters.SELECTED_PLACEMARK
+    }
+  },
+  methods: {
+    addPlacemark(coordinates) {
+      this.$store.commit('SET_PLACEMARK', coordinates)
+    },
+    selectPlacemark(coordinates) {
+      this.mapCenter = coordinates
+      this.$store.commit('SELECT_PLACEMARK', coordinates)
+    },
+    onMapClick(e) {
+      this.addPlacemark(e.get('coords'))
+    },
+    onMarkerClick(e) {
+      const target = e.get('target')
+      this.selectPlacemark(target.geometry._coordinates)
+    },
+    isActivePlacemark(item){
+      if(item.coordinates[0] === this.selectedPlacemark[0]){
+        if(item.coordinates[1] === this.selectedPlacemark[1]){
+          return true;
+        }
+      }
+      return false;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  padding: 0;
+}
+.map{
+  width: 100%;
+  height: 100vh;
+}
+.placemarks{
+  position: fixed;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  top: 30px;
+  left: 30px;
+  min-width: 300px;
+  max-height: calc(100vh - 60px);
+}
+.placemarks__list{
+  max-height: calc(100vh - 150px);
+  overflow-y: auto;
+  padding: 0;
+
+  li{
+    list-style: none;
+    padding: 10px;
+
+    &:hover, &.active{
+      background-color: #eee;
+    }
+  }
+  h6{
+    font-size: .8rem;
+    margin-top: 15px;
+    margin-bottom: 5px;
+  }
+  span{
+    font-size: .7rem;
+    color: #ccc;
+  }
+}
+.placemarks__title{
+    background-color: #4f88c4;
+    width: calc(100% - 30px);
+    padding: 15px;
+    border-radius: 8px 8px 0 0;
+
+  h2{
+    color: #fff;
+    margin: 0;
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,44 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    settings: {
+      localization: "ru_RU"
+    },
+    localization: {
+      ru_RU: {
+        marker: "Маркер",
+        list_title: "Список маркеров"
+      }
+    },
+    yamapSettings: {
+      apiKey: '8ad9575f-e3db-444a-afed-bb7b83a5329a',
+      coordorder: 'latlong',
+      version: '2.1',
+    },
+    mapCenter: [55.76, 37.64],
+    placemarks: [],
+    selectPlacemark: []
+  },
+  getters: {
+    YAMAP_SETTINGS: state => {
+      let settings = state.yamapSettings
+      settings.lang = state.settings.localization
+      return settings
+    },
+    MAP_CENTER: state => state.mapCenter,
+    PLACEMARKS: state => state.placemarks,
+    SELECTED_PLACEMARK: state => state.selectPlacemark,
+    LOCALIZATION: state => state.localization[state.settings.localization]
   },
   mutations: {
+    SET_PLACEMARK: (state, coordinates) => {
+      state.placemarks.push({
+        coordinates
+      })
+    },
+    SELECT_PLACEMARK: (state, coordinates) => {
+      state.selectPlacemark = coordinates
+    }
   },
   actions: {
   },
